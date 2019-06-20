fc492dae0b080cacd95cfbf7afaa7bf8753d758a
Morgan Smith <morgansmith0219@gmail.com>
2019-06-20 23:11:50 +0000

    adding description

--- db/migrate/20190620225929_create_places.rb
+++ db/migrate/20190620225929_create_places.rb
@@ -1,8 +1,8 @@
-class CreatePlaces < ActiveRecord::Migration[5.2]
+class CreatePlaces < ActiveRecord::Migration[5.0]
   def change
     create_table :places do |t|
       t.string :name
-      t.string :text
+      t.text :description
       t.string :address
       t.timestamps
     end
